7cc1ebc0dfd9222292df542ea8c7bbaee98636db
harshy250 <yadav.harshgst@gmail.com>
2021-08-01 13:34:51 +0530

    Edit User profile functionality added

--- users/signals.py
+++ users/signals.py
@@ -14,6 +14,15 @@ def createProfile(sender, instance, created, **kwargs):
             name = user.first_name
         )
 
+def updateUser(sender, instance, created, **kwargs):
+    profile = instance
+    user = profile.user
+    if created == False:
+        user.first_name = profile.name
+        user.username = profile.username
+        user.email = profile.email
+        user.save()
+
 def deleteUser(sender, instance, **kwargs):
     try:
         user = instance.user 
@@ -22,5 +31,6 @@ def deleteUser(sender, instance, **kwargs):
         print('User deletion is called from CASCADE')
 
 post_save.connect(createProfile, sender=User)
+post_save.connect(updateUser, sender=Profile) #Whenever profile is updated, trigger this signal
 post_delete.connect(deleteUser, sender=Profile)
 
--- users/views.py
+++ users/views.py
@@ -4,7 +4,7 @@ from django.contrib.auth import login, authenticate, logout
 from django.contrib import messages
 from django.contrib.auth.models import User
 from .models import Profile
-from .forms import CustomUserCreationForm
+from .forms import CustomUserCreationForm, ProfileForm
 from django.contrib.auth.decorators import login_required
 
 
@@ -54,7 +54,7 @@ def registerUser(request):
             messages.success(request, 'Successfully Registered!')
 
             login(request, user)
-            return redirect('profiles')
+            return redirect('edit-account')
         
         else:
             messages.error(request, 'Registration Failed!')
@@ -89,3 +89,19 @@ def userAccount(request):
 
     context = {'profile':profile, 'skills':skills, 'projects':projects}
     return render(request, 'users/account.html', context)
+
+@login_required(login_url='login')
+def editAccount(request):
+    profile = request.user.profile
+    form = ProfileForm(instance=profile)
+
+    if request.method == 'POST':
+        form = ProfileForm(request.POST, request.FILES, instance=profile)
+
+        if form.is_valid():
+            form.save()
+
+            return redirect('account')
+
+    context = {'form':form}
+    return render(request, 'users/profile_form.html', context)
